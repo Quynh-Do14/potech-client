1e4cbf9a8f32a236ecb904fe47da55be56487929
Quynh Do <doducquynh14022000@gmail.com>
2026-03-11 23:35:06 +0700

    slug

--- src/app/san-pham/[slug]/components/blogRandom.tsx
+++ src/app/san-pham/[slug]/components/blogRandom.tsx
@@ -45,7 +45,7 @@ const BlogInProductSlug = () => {
                     listBlog.map((article, index) => (
                         <article key={index} className={styles.recentCard}>
                             <Link
-                                href={`${ROUTE_PATH.BLOG}/${convertSlug(article?.title)}-${article?.id}.html`}
+                                href={`${ROUTE_PATH.BLOG}/${article?.slug}.html`}
                                 className={styles.recentLink}
                             >
                                 <div className={styles.recentImage}>
